7f277f571d14289a1239ab27af3d5e0030558f96
Linus Östberg <linus.ostberg@scilifelab.uu.se>
2022-12-17 12:12:43 +0100

    Add template

---
+++ 2022/template.py
@@ -0,0 +1,46 @@
+import requests
+
+day = 1
+
+def parse(indata):
+    data_rows = raw.split("\n")
+    data = [row for row in data_rows if row]
+    return data
+
+
+def calc(data):
+    score = 0
+    return score
+
+
+def calc2(data):
+    score = 0
+    return score
+
+
+test_data = """
+"""
+
+test_data = [row for row in test_data.split("\n") if row]
+
+testing = True
+if testing:
+    res1 = calc(test_data)
+    res2 = calc2(test_data)
+    ans1 = -1
+    ans2 = -1
+    print(f"Test part 1: {res1} ({ans1}){'   !!!' if res1 != ans1 else ''}")
+    print(f"Test part 2: {res2} ({ans2}){'   !!!' if res2 != ans2 else ''}")
+
+active = True
+if active:
+    cookies = {"session": open("cookie.dat").read()}
+    try:
+        raw = open(f"{day}.txt").read()
+    except FileNotFoundError:
+        raw = requests.get(f"https://adventofcode.com/2022/day/{day}/input", cookies=cookies).text
+        with open(f"{day}.txt", "w") as outfile:
+            outfile.write(raw)
+    data = parse(raw)
+    print(f"Part 1: {calc(data)}")
+    print(f"Part 2: {calc2(data)}")
