205bd7a29f74f9de247d52dddbdce4aabd989bd8
Linus Östberg <linus@oestberg.dev>
2025-12-07 11:12:57 +0100

    7 2025

---
+++ 2025/7.py
@@ -0,0 +1,139 @@
+import functools
+import time
+
+import requests
+
+day = 7
+part1 = True
+part2 = True
+testing = True
+active = True
+timings = True
+
+test_data = """
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+"""
+
+test_data2 = test_data
+
+test_ans1 = 21
+test_ans2 = 40
+
+
+def parse(indata):
+    data_rows = indata.split("\n")
+    data = [row for row in data_rows if row]
+    return data
+
+
+def calc(data):
+    ans = 0
+    level = 1
+    x_pos = {data[0].index("S")}
+    while level < len(data):
+        new_x_pos = set()
+        for x in x_pos:
+            if data[level][x] == "^":
+                ans += 1
+                if x-1 >= 0:
+                    new_x_pos.add(x-1)
+                if x+1 < len(data[0]):
+                    new_x_pos.add(x+1)
+            else:
+                new_x_pos.add(x)
+        x_pos = new_x_pos
+        level += 1
+    return ans
+
+
+def calc2(data):
+    @functools.cache
+    def step(level, x):
+        ans = 0
+        if level + 1 == len(data):
+            return ans
+        if data[level][x] == "^":
+            ans += 1
+            if x - 1 >= 0:
+                ans += step(level+1, x-1)
+            if x + 1 < len(data[0]):
+                ans += step(level + 1, x + 1)
+        else:
+            ans += step(level + 1, x)
+        return ans
+    
+    x = data[0].index("S")
+    ans = step(0, x) + 1
+    return ans
+
+
+start_total = time.perf_counter()
+if testing:
+    if part1:
+        test_data_p = parse(test_data)
+        start_test_part1 = time.perf_counter()
+        res1 = calc(test_data_p)
+        end_test_part1 = time.perf_counter()
+        print(f"Test part 1: {res1} ({test_ans1}){'   !!!' if res1 != test_ans1 else ''}")
+
+    if part2:
+        test_data_p2 = parse(test_data2)
+        start_test_part2 = time.perf_counter()
+        res2 = calc2(test_data_p2)
+        end_test_part2 = time.perf_counter()
+        print(f"Test part 2: {res2} ({test_ans2}){'   !!!' if res2 != test_ans2 else ''}")
+
+if active:
+    with open("cookie.dat") as f:
+        cookies = {"session": f.read()}
+    try:
+        with open(f"{day}.txt") as f:
+            raw = f.read()
+    except FileNotFoundError:
+        raw = requests.get(f"https://adventofcode.com/2025/day/{day}/input", cookies=cookies).text
+        with open(f"{day}.txt", "w") as outfile:
+            outfile.write(raw)
+    if part1:
+        data = parse(raw)
+        start_part1 = time.perf_counter()
+        ans = calc(data)
+        end_part1 = time.perf_counter()
+        print(f"Part 1: {ans}")
+
+    if part2:
+        data = parse(raw)
+        start_part2 = time.perf_counter()
+        ans = calc2(data)
+        end_part2 = time.perf_counter()
+        print(f"Part 2: {ans}")
+
+end_total = time.perf_counter()
+
+if timings:
+    print("Runtime:")
+    if part1:
+        if testing:
+            print(f"Test 1: {end_test_part1 - start_test_part1} seconds")
+        if active:
+            print(f"Part 1: {end_part1 - start_part1} seconds")
+    if part2:
+        if testing:
+            print(f"Test 2: {end_test_part2 - start_test_part2} seconds")
+        if active:
+            print(f"Part 2: {end_part2 - start_part2} seconds")
+    print(f"Total: {end_total - start_total} seconds")
